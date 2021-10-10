881b9f88a98ce2075f191f67f61fb692a52c53a2
Haroon Sa <50052779+haroon3694@users.noreply.github.com>
2021-10-10 10:44:38 +0530

    js js js

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -6,6 +6,22 @@ const api ={
 }
 
 function App() {
+
+  const dateBuilder = (d) =>{
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let days = ["Monday", "tuesday", "Wednesday","Thursday","Friday","Saturday","Sunday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+
+    return [day, date, month, year];
+  } 
+
+
+
   return (
     <div className="app">
       <main>
@@ -15,6 +31,18 @@ function App() {
           className="search-bar"
           placeholder="search.."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Chennai</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15°c
+          </div>
+          <div className="weather">
+            Sunny
+          </div>
         </div>
       </main>
     </div>
